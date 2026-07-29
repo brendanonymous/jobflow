651dc66968e935ccdc1fc91cd14320d3805fbf92
Brendan Lauck <brendanlauck@gmail.com>
2026-07-28 20:43:29 -0700

    nits

--- backend/src/visualizations/utils.py
+++ backend/src/visualizations/utils.py
@@ -1,17 +1,1 @@
-import csv
-from src.visualizations.application_snapshot import ApplicationSnapshot
-
-'''
-TODO:
-get_job_applications is a temporary POC function.
-It will be replaced by real DB fetching using an ORM.
-'''
-def get_job_applications(filename: str = "backend/src/visualizations/applications.csv") -> list[ApplicationSnapshot]:
-    job_applications = []
-    with open(filename, newline='') as file:
-        reader = csv.reader(file)
-        for job_application in reader:
-            company, date_applied, status = job_application[0], job_application[1], job_application[2]
-            job_applications.append(ApplicationSnapshot(company, date_applied, status))
-    
-    return job_applications
+pass
--- frontend/src/api/applications.js
+++ frontend/src/api/applications.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000"; // TODO: rm before prod
+const BASE_URL = "http://localhost:8000"; // TODO: update to env var before deployment
 
 export const fetchApplications = async () => {
   const response = await fetch(`${BASE_URL}/applications`);
